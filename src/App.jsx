fac382d9a525060e1de317e92f2a1c0df5c3b1e8
thomasbrasil46 <thomasbrasilpb@gmail.com>
2025-02-27 19:21:51 -0300

    Criando páginas de overview e de clientes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { Routes } from "react-router-dom"
+import OverviewPage from "./pages/OverviewPage"
+import ProductsPage from "./pages/ProductsPage"
+
 function App() {
-  return <div className='text-red-500 text-4xl bg-blue-500'>Chama</div>
+  return (
+    <div className='flex h-screen bg-gray-900 text-gray-100 overflow-hidden'>
+
+      <Routes>
+        <Route path='/' element={<OverviewPage/>}/>
+        <Route path='/products' element={<ProductsPage/>}/>
+      </Routes>
+    </div>
+  )
 }
 
 export default App
