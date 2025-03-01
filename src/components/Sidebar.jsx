ae511be8ab3907d83e5529214599ceadef3103d0
thomasbrasil46 <thomasbrasilpb@gmail.com>
2025-03-01 16:29:03 -0300

    criando e estilizando sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
-import { BarChart2 } from "lucide-react"
+import { BarChart2, Menu } from "lucide-react"
 import { useState } from "react"
 import { motion } from "framer-motion"
+import { Link } from "react-router-dom";
 
 const SIDEBAR_ITEMS = [
     {
@@ -12,7 +13,8 @@ const SIDEBAR_ITEMS = [
     {name: "Feedback", icon:BarChart2, color:"#f59e0b", path:"/Feedback"},
     {name: "Clients", icon:BarChart2, color:"#ef4444", path:"/Clients"},
     {name: "Search", icon:BarChart2, color:"#6366f1", path:"/Search"},
-]
+];
+
 const Sidebar = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true)
   return (
@@ -22,14 +24,25 @@ const Sidebar = () => {
     >
         <div className='h-full bg-gray-800 bg-opacity-50 backdrop-blur-md p-4 flex flex-col border-r border-gray-700'>
             <motion.button
-            whileHover={{scale: 1.1}}
-            whileTap={{scale: 0.9}}
-            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-            className='p-2 rounded-full hover-bg-gray-700 transition-colors max-w-fit'
-            >
-                <Menu size={24}/>
-
+                whileHover={{scale: 1.1}}
+                whileTap={{scale: 0.9}}
+                onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+                className='p-2 rounded-full hover:bg-gray-700 transition-colors max-w-fit'
+            >            
+            <Menu size={24} /> 
             </motion.button>
+
+            <nav className='mt-8 flex-grow'>
+                {SIDEBAR_ITEMS.map((item, index) => (
+                    <Link key={item.href} to={item.href}>
+                        <motion.div
+                            className='flex items-center p-4 text-sm font-medium rounded-lg hover:bg-gray-700 transition-colors mb-2'>
+                                <item.icon size={20} style={{color: item.color, minWidth: "20px"}} />
+                        </motion.div>
+                    </Link>
+                ))}
+
+            </nav>
             
         </div>          
     </motion.div>
