98c9e9327bde0b5e946044943059468c52954267
thomasbrasil46 <thomasbrasilpb@gmail.com>
2025-03-07 02:41:50 -0300

    implementação dos stats

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from '../components/common/Header'
 import { motion } from 'framer-motion'
+import StatsCards from '../components/common/StatsCards'
+import { BarChart2, ShoppingBag, Users, Zap } from 'lucide-react'
 
 const OverviewPage = () => {
   return (
@@ -16,10 +18,10 @@ const OverviewPage = () => {
 					animate={{ opacity: 1, y: 0 }}
 					transition={{ duration: 1 }}
         >
-          <StatCard name='Total Sales' icon={Zap} value='$12,345' color='#6366F1' />
-					<StatCard name='New Users' icon={Users} value='1,234' color='#8B5CF6' />
-					<StatCard name='Total Products' icon={ShoppingBag} value='567' color='#EC4899' />
-					<StatCard name='Conversion Rate' icon={BarChart2} value='12.5%' color='#10B981' />
+          <StatsCards name='Total Sales' icon={Zap} value='$12,345' color='#6366F1' />
+					<StatsCards name='New Users' icon={Users} value='1,234' color='#8B5CF6' />
+					<StatsCards name='Total Products' icon={ShoppingBag} value='567' color='#EC4899' />
+					<StatsCards name='Conversion Rate' icon={BarChart2} value='12.5%' color='#10B981' />
 
         </motion.div>
 
