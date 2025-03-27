9f1c13cebee6f2b9d8b4a0cedb31322ecf3a352c
thomasbrasil46 <thomasbrasilpb@gmail.com>
2025-03-27 17:44:57 -0300

    integrando backend ao projeto

---
+++ CadastroClientes/CadastroClientes/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadastroColaboradores.Models
+{
+    public class Colaborador
+    {
+        public int ColaboradorId { get; set; }
+        [Required]
+        [StringLength(400)]
+        public string Nome { get; set; }
+        [Required]
+        [EmailAddress]
+        [StringLength(400)]
+        public string Email { get; set; }
+        [Required]
+        public int Idade { get; set; }
+    }
+}
