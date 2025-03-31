ba67bddcbfa556cca8e247c9e3aea2ef12349721
thomasbrasil46 <thomasbrasilpb@gmail.com>
2025-03-31 12:18:48 -0300

    implementação de service

--- CadastroClientes/Services/ClienteService.cs
+++ CadastroClientes/Services/ClienteService.cs
@@ -1,6 +1,37 @@
-﻿namespace CadastroClientes.Services
+﻿using CadastroCliente.Models;
+
+namespace CadastroClientes.Services
 {
-    public class ClienteService
+    public class ClienteService : IClienteService
     {
+        public Task CreateCliente(ClienteTest cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCliente(ClienteTest cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ClienteTest> GetCliente(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ClienteTest>> GetClienteS()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ClienteTest>> GetClientesByName(string nome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCliente(ClienteTest cliente)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
