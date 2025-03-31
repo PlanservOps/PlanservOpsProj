b2a2ebd3b9f652450abca32bff88a40b2039de29
thomasbrasil46 <thomasbrasilpb@gmail.com>
2025-03-31 00:07:42 -0300

    populando tabela

--- CadastroClientes/Context/AppDbContext.cs
+++ CadastroClientes/Context/AppDbContext.cs
@@ -11,5 +11,33 @@ namespace CadastroCliente.Context
 
         public DbSet<ClienteTest> ClienteTest { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ClienteTest>().HasData(
+                new ClienteTest
+                {
+                    ColaboradorId = 1,
+                    Posto = "Arvoredo",
+                    NomeResponsavel = "Antônio Henrique",
+                    ContatoResponsavel = "83981295876",
+                    Funcao = Models.ClienteTest.FuncaoEnum.Sindico, 
+                    Endereco = "RUA DOMINGOS MOROSO, S/N MIRAMAR EM JOAO PESSOA NO ESTADO DA PB, CEP:58043-170",
+                    Bairro = "Miramar",
+                    FuncoesTerceirizadas = "NPS"
+                },
+                new ClienteTest
+                {
+                    ColaboradorId = 2,
+                    Posto = "Imperial Bessa",
+                    NomeResponsavel = "Mariana",
+                    ContatoResponsavel = "83981295876",
+                    Funcao = Models.ClienteTest.FuncaoEnum.Sindica,
+                    Endereco = "AVENIDA PRESIDENTE AFONSO PENA, 382, BESSA, EM JOAO PESSOA NO ESTADO DA PB, CEP: 58035-030",
+                    Bairro = "Bessa",
+                    FuncoesTerceirizadas = "NPS"
+                }
+                );
+        }
+
     }
 }
