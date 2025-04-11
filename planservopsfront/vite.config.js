94f1d69a6dac5b0a52bb3bd93ebaea7eec3ea80c
thomasbrasil46 <thomasbrasilpb@gmail.com>
2025-04-11 19:13:01 -0300

    configs para deploy

--- planservopsfront/vite.config.js
+++ planservopsfront/vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    tailwindcss()],
+    react()],
+    build: {
+      minify: 'esbuild',
+      sourcemap: false,
+      outDir: 'dist',
+      chunkSizeWarningLimit: 1000,
+    },
+    optimizeDeps: {
+      include: ['react', 'react-dom', 'react-router-dom'],
+    }
 })
+
