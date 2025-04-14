9c1ce17397d61ef4db9675f1b8aaecfed4292e1a
thomasbrasil46 <thomasbrasilpb@gmail.com>
2025-04-14 15:26:45 -0300

    vite config

--- planservopsfront/vite.config.js
+++ planservopsfront/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/', 
   plugins: [react()],
   build: {
     minify: 'esbuild',
@@ -12,5 +12,5 @@ export default defineConfig({
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom'],
-  },
+  }
 })
