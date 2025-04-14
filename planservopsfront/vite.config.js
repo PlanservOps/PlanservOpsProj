cc7b1aad8a9c7167b77f50228c0693b8d7781d6d
thomasbrasil46 <thomasbrasilpb@gmail.com>
2025-04-14 09:18:41 -0300

    configs adicionais

--- planservopsfront/vite.config.js
+++ planservopsfront/vite.config.js
@@ -3,16 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react()],
-    build: {
-      minify: 'esbuild',
-      sourcemap: false,
-      outDir: 'dist',
-      chunkSizeWarningLimit: 1000,
-    },
-    optimizeDeps: {
-      include: ['react', 'react-dom', 'react-router-dom'],
-    }
+  plugins: [react()],
+  build: {
+    minify: 'esbuild',
+    sourcemap: false,
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1000,
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom'],
+  },
 })
-
