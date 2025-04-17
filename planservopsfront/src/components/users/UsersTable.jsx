6d28d719a29c3126dd23a83d80dbb61a71949098
thomasbrasil46 <thomasbrasilpb@gmail.com>
2025-04-17 18:06:47 -0300

    fix endpoints

--- planservopsfront/src/components/users/UsersTable.jsx
+++ planservopsfront/src/components/users/UsersTable.jsx
@@ -39,7 +39,7 @@ function UsersTable() {
 	useEffect(() => {
 		const fetchUsers = async () => {
 			try {
-				const response = await fetch(baseUrl);
+				const response = await fetch(`${baseUrl}/Clientes`);
 				if (!response.ok) {
 					throw new Error(`Erro: ${response.status} - ${response.statusText}`);
 				}
@@ -56,7 +56,7 @@ function UsersTable() {
 	
 	const addUser = async () => {
 		try {
-			const response = await fetch(baseUrl, {
+			const response = await fetch(`${baseUrl}/Clientes`, {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
