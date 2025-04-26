0df56e67e76e6525e491dfd1c3e5321d2fd687a8
thomasbrasil46 <thomasbrasilpb@gmail.com>
2025-04-26 01:23:39 -0300

    att bg img

--- planservopsfront/src/pages/LoginPage.jsx
+++ planservopsfront/src/pages/LoginPage.jsx
@@ -3,8 +3,8 @@ import Login from '../components/leadsAccess/Login'
 const LoginPage = () => {    
     
   return ( 
-        <main className='flex justify-center items-center w-screen min-h-screen bg-gray-900 background-image `Url(/src/assets/images/bgLoginTemplate.jpg)` background-cover background-center'>
-            <div className='w-full max-w-md'>
+        <main className='flex justify-center items-center w-screen min-h-screen bg-[url(/src/assets/images/bgLoginTemplate.jpg)] background-cover background-center'>
+            <div className='w-full max-w-md bg-trasparent'>
                 <Login />
             </div>
         </main>
