e9c8265438f47989cbed866845a0fa36aeebe31e
thomasbrasil46 <thomasbrasilpb@gmail.com>
2025-05-07 05:54:18 -0300

    last test

--- planservopsfront/vite.config.js
+++ planservopsfront/vite.config.js
@@ -1,25 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: './', 
-  plugins: [react()],
-  build: {
-    minify: 'esbuild',
-    sourcemap: false,
-    outDir: 'dist',
-    chunkSizeWarningLimit: 1000,
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom'],
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://planservopsprojapi-production.up.railway.app',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Carrega as variáveis de ambiente do arquivo correspondente (ex: .env.production)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: './',
+    plugins: [react()],
+    build: {
+      minify: 'esbuild',
+      sourcemap: false,
+      outDir: 'dist',
+      chunkSizeWarningLimit: 1000,
+    },
+    optimizeDeps: {
+      include: ['react', 'react-dom', 'react-router-dom'],
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://planservopsprojapi-production.up.railway.app',
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  }
-})
+    define: {
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
+    },
+  };
+});
