7fd72e191bc6cfeac40ab6210cfb7def486f3e08
thomasbrasil46 <thomasbrasilpb@gmail.com>
2025-05-14 21:31:55 -0300

    Criação de service e interface de service

---
+++ CadastroClientes/Services/FormularioService.cs
@@ -0,0 +1,24 @@
+﻿using CadastroCliente.Context;
+using CadastroClientes.Models;
+
+namespace CadastroClientes.Services
+{
+    public class FormularioService : IFormularioService
+    {
+        private readonly AppDbContext _context;
+        public FormularioService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateFormulario(FormularioOperacional formulario)
+        {
+            _context.FormularioOperacional.Add(formulario);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<FormularioOperacional> GetFormulario(int id)
+        {
+            var formulario = await _context.FormularioOperacional.FindAsync(id);
+            return formulario;
+        }
+    }        
+}
---
+++ CadastroClientes/Services/IFormularioService.cs
@@ -0,0 +1,10 @@
+﻿using CadastroClientes.Models;
+
+namespace CadastroClientes.Services
+{
+    public interface IFormularioService
+    {
+        Task CreateFormulario(FormularioOperacional formulario);
+        Task<FormularioOperacional> GetFormulario(int id);
+    }
+}
