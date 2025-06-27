8acaa75f15689064c9359b99ac0089be126a49a5
thomasbrasil46 <thomasbrasilpb@gmail.com>
2025-06-26 23:41:39 -0300

    fix: improve code formatting and enhance readability across multiple components

--- planservopsfront/src/components/overview/IndexNpsChart.jsx
+++ planservopsfront/src/components/overview/IndexNpsChart.jsx
@@ -1,55 +1,64 @@
 import { motion } from "framer-motion";
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend, Cell } from "recharts";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  Legend,
+  Cell,
+} from "recharts";
 
 const COLORS = ["#6366F1", "#8B5CF6", "#EC4899", "#10B981", "#F59E0B"];
 
 const INDEX_NPS_DATA = [
-	{ name: "Preventivas", value: 25 },
-	{ name: "Corretivas", value: 20 },
-	{ name: "Não Atendidas", value: 15 },
-	{ name: "Totais", value: 30 },
+  { name: "Preventivas", value: 25 },
+  { name: "Corretivas", value: 20 },
+  { name: "Não Atendidas", value: 15 },
+  { name: "Totais", value: 30 },
 ];
 
 const IndexNpsChart = () => {
   return (
     <motion.div
-			className='bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 lg:col-span-2 border border-gray-700'
-			initial={{ opacity: 0, y: 20 }}
-			animate={{ opacity: 1, y: 0 }}
-			transition={{ delay: 0.4 }}
-		>
-            <h2 className='text-lg font-medium mb-4 text-gray-100'>Índice de Ocorrências</h2>
+      className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 lg:col-span-2 border border-gray-700"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.4 }}
+    >
+      <h2 className="text-lg font-medium mb-4 text-gray-100">
+        Índice de Ocorrências
+      </h2>
 
-            <div className="h-80">
-                <ResponsiveContainer>
-                    <BarChart
-                        data={INDEX_NPS_DATA}
-                    >
-                        <CartesianGrid strokeDasharray='3 3' stroke='#4B5563' />
-						<XAxis dataKey='name' stroke='#9CA3AF' />
-						<YAxis stroke='#9CA3AF' />
-						<Tooltip
-							contentStyle={{
-								backgroundColor: "rgba(31, 41, 55, 0.8)",
-								borderColor: "#4B5563",
-							}}
-							itemStyle={{ color: "#E5E7EB" }}
-						/>
-                        <Legend />
-                        <Bar
-                            dataKey={"value"} fill='#8884d8'
-                        >
-                            {INDEX_NPS_DATA.map((entry, index) => ( 
-                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                            ))}
-                        </Bar>
-
-                    </BarChart>
-                </ResponsiveContainer>
-            </div>
-            
+      <div className="h-80">
+        <ResponsiveContainer>
+          <BarChart data={INDEX_NPS_DATA}>
+            <CartesianGrid strokeDasharray="3 3" stroke="#4B5563" />
+            <XAxis dataKey="name" stroke="#9CA3AF" />
+            <YAxis stroke="#9CA3AF" />
+            <Tooltip
+              contentStyle={{
+                backgroundColor: "rgba(31, 41, 55, 0.8)",
+                borderColor: "#4B5563",
+              }}
+              itemStyle={{ color: "#E5E7EB" }}
+            />
+            <Legend />
+            <Bar dataKey={"value"} fill="#8884d8">
+              {INDEX_NPS_DATA.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default IndexNpsChart
+export default IndexNpsChart;
